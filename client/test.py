63f9f36d84bfb648ba1cd96fd04da96bfacdff19
Ksaiten <maren.kucza@gmx.de>
2016-05-23 19:04:43 +0200

    Testprogramm für die Client Seite. Vorerst Beispiel von Adafruit Dokumentation

---
+++ client/test.py
@@ -0,0 +1,65 @@
+# verwendet Python 2.7, da die Adafruit lib nur für 2.7 verfügbar war
+
+import Adafruit_BluefruitLE
+from Adafruit_BluefruitLE.services import UART
+
+
+ble = Adafruit_BluefruitLE.get_provider()
+ble.initialize()
+ble.run_mainloop_with()
+
+
+def main():
+    ble.clear_cached_data()
+    adapter = ble.get_default_adapter()
+    adapter.power_on()
+    print('Using adapter: {0}'.format(adapter.name))
+
+    print('Disconnecting any connected UART devices...')
+    UART.disconnect_devices()
+
+    # Scan for UART devices.
+    print('Searching for UART device...')
+    try:
+        adapter.start_scan()
+        # Search for the first UART device found (will time out after 60 seconds
+        # but you can specify an optional timeout_sec parameter to change it).
+        device = UART.find_device()
+        if device is None:
+            raise RuntimeError('Failed to find UART device!')
+    finally:
+        # Make sure scanning is stopped before exiting.
+        adapter.stop_scan()
+
+    print('Connecting to device...')
+    device.connect()  # Will time out after 60 seconds, specify timeout_sec parameter
+    # to change the timeout.
+
+    # Once connected do everything else in a try/finally to make sure the device
+    # is disconnected when done.
+    try:
+        # Wait for service discovery to complete for the UART service.  Will
+        # time out after 60 seconds (specify timeout_sec parameter to override).
+        print('Discovering services...')
+        UART.discover(device)
+
+        # Once service discovery is complete create an instance of the service
+        # and start interacting with it.
+        uart = UART(device)
+
+        # Write a string to the TX characteristic.
+        uart.write('Hello world!\r\n')
+        print("Sent 'Hello world!' to the device.")
+
+        # Now wait up to one minute to receive data from the device.
+        print('Waiting up to 60 seconds to receive data from the device...')
+        received = uart.read(timeout_sec=60)
+        if received is not None:
+            # Received data, print it out.
+            print('Received: {0}'.format(received))
+        else:
+            # Timeout waiting for data, None is returned.
+            print('Received no data!')
+    finally:
+        # Make sure device is disconnected on exit.
+        device.disconnect()
