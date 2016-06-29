1384b1e3859d00e309acd377f53d73cf100d4ead
Andrea Bittner <git@andbit.de>
2016-06-29 23:33:19 +0200

    update proTrinket code without code

--- microcontroller/orientation_via_bluetooth_4proTrinket/orientation_via_bluetooth_4uno/orientation_via_bluetooth_4uno.ino
+++ microcontroller/orientation_via_bluetooth_4proTrinket/orientation_via_bluetooth_4uno/orientation_via_bluetooth_4uno.ino
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <SPI.h>
-//#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
-//  #include <SoftwareSerial.h>
-//#endif
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+  #include <SoftwareSerial.h>
+#endif
 
 /* SENSORS */
 #include <Wire.h>
@@ -48,23 +48,19 @@ Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
 
 /* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
 /* PRO TRINKET */
-// Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+ Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
 /* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
 /* UNO */
-Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
+/*Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
                              BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
-                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);  
+                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);*/
 
 // A small helper
 void error(const __FlashStringHelper*err) {
- // Serial.println(err);
+  Serial.println(err);
   while (1);
 }
-
-// Counter
-int loopnr =0;
-bool sending = false;
 
 /**************************************************************************/
 /* Initialises all the sensors */
@@ -74,13 +70,13 @@ void initSensors()
   if(!accel.begin())
   {
     /* There was a problem detecting the LSM303 ... check your connections */
- //   Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
     while(1);
   }  
   if(!mag.begin())
   {
     /* There was a problem detecting the LSM303 ... check your connections */
-//    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
     while(1);
   }
 }
@@ -90,22 +86,22 @@ void initSensors()
 /**************************************************************************/
 void setup(void)
 {
-//  while (!Serial);  // required for Flora & Micro
-//  delay(500);
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
   Serial.begin(115200);
 
   /* Initialise the module */
- // Serial.print(F("Initialising the Bluefruit LE module: "));
+  Serial.print(F("Initialising the Bluefruit LE module: "));
   if ( !ble.begin(VERBOSE_MODE) )
   {
     error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
   }
- // Serial.println( F("OK!") );
+  Serial.println( F("OK!") );
 
   if ( FACTORYRESET_ENABLE )
   {
     /* Perform a factory reset to make sure everything is in a known state */
-//    Serial.println(F("Performing a factory reset: "));
+    Serial.println(F("Performing a factory reset: "));
     if ( ! ble.factoryReset() ){
       error(F("Couldn't factory reset"));
     }
@@ -113,10 +109,10 @@ void setup(void)
   /* Disable command echo from Bluefruit */
   ble.echo(false);
 
-//  Serial.println("Requesting Bluefruit info:");
+  Serial.println("Requesting Bluefruit info:");
   ble.info();
-//  Serial.println(F("Use Adafruit Bluefruit LE app to connect in UART mode"));
-//  Serial.println();
+  Serial.println(F("Use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println();
 
   ble.verbose(false);  // debug info 
 
@@ -129,13 +125,13 @@ void setup(void)
   if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
   {
     // Change Mode LED Activity
- //   Serial.println(F("******************************"));
- //   Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    Serial.println(F("******************************"));
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
     ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
- //   Serial.println(F("******************************"));
+    Serial.println(F("******************************"));
   }
 
-//  Serial.println(F("Adafruit 9 DOF Pitch/Roll/Heading ")); Serial.println("");
+  Serial.println(F("Adafruit 9 DOF Pitch/Roll/Heading ")); Serial.println("");
   initSensors();
 }
 
@@ -144,81 +140,65 @@ void setup(void)
 /**************************************************************************/
 void loop(void)
 {
-  if(ble.isConnected()){
-    
-    // Check incomming Commands
-    ble.println("AT+BLEUARTRX");
-    if (ble.readline()) {
-      /*private static class  microCommand {
-            public static int START    = 1;
-            public static int STOP     = 2;
-            public static int RESET    = 3;
-        }*/
-      Serial.println(ble.buffer);
-    
-      if ( strcmp(ble.buffer, "1") == 0) { 
-          //START
-          sending = true;
-          ble.print("ble: start");
-          loopnr = 0;
-        } else if ( strcmp(ble.buffer, "2") == 0) {
-          //STOP
-          sending = false;
-        } else if ( strcmp(ble.buffer, "3") == 0) {
-          //RESET
-          sending = false;
-          loopnr = 0;
-          ble.factoryReset();
-          ble.echo(false);
-          while (! ble.isConnected()) {
-            delay(500);
-          }
-       }
-    }
- 
-  delay(60);
-  if (sending) 
+  sensors_event_t accel_event;
+  sensors_event_t mag_event;
+  sensors_vec_t   orientation;
+
+  /* Calculate pitch and roll from the raw accelerometer data */
+  accel.getEvent(&accel_event);
+  if (dof.accelGetOrientation(&accel_event, &orientation))
   {
-    sensors_event_t accel_event;
-    sensors_event_t mag_event;
-    sensors_vec_t   orientation;
-  
-    /* Calculate pitch and roll from the raw accelerometer data */
-    accel.getEvent(&accel_event);
-    if (dof.accelGetOrientation(&accel_event, &orientation))
-    {
-     /* OUTPUT FORMAT BLE {[loopnr, time, roll, pitch, yaw, (accel) x, y z],..}  */
-      ble.print("AT+BLEUARTTX=");
-      ble.print("{");
-      ble.print(loopnr);
-      ble.print(",");
-      ble.print(millis());
-      ble.print(",");
-      ble.print(orientation.roll);
-      ble.print(",");
-      ble.print(orientation.pitch);
-      ble.print(",");
-      
-      /* Calculate the heading using the magnetometer */
-      mag.getEvent(&mag_event);
-      if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation)){
-          ble.print(orientation.heading);
-          ble.print(",");  
-      }
-        
-      ble.print(accel_event.acceleration.x);
-      ble.print(",");
-      ble.print(accel_event.acceleration.y);
-      ble.print(",");
-      ble.print(accel_event.acceleration.z);
-      ble.println("},");
-     loopnr++;
+
+    
+   /* OUTPUT FORMAT BLE 
+    {[time, roll, pitch, yaw, (accel) x, y z],..}
+    */
+   
+    ble.print("AT+BLEUARTTX=");
+    ble.print("[");
+    ble.print(millis());
+    ble.print(",");
+    ble.print(orientation.roll);
+    ble.print(",");
+    ble.print(orientation.pitch);
+    ble.print(",");
+    
+    /* Calculate the heading using the magnetometer */
+    mag.getEvent(&mag_event);
+    if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation)){
+        ble.print(orientation.heading);
+        ble.print(",");  
     }
       
-    ble.waitForOK();
-    
- } else {
-    delay(1000);
-   }
- }
+    ble.print(accel_event.acceleration.x);
+    ble.print(", ");
+    ble.print(accel_event.acceleration.y);
+    ble.print(", ");
+    ble.print(accel_event.acceleration.z);
+    ble.println("],");
+
+    Serial.print(millis());
+    Serial.print(F(", Orientation  Roll: "));
+    Serial.print(orientation.roll);
+    Serial.print(F("; "));
+    Serial.print(F("Pitch: "));
+    Serial.print(orientation.pitch);
+    Serial.print(F(" Heading: "));
+    Serial.print(orientation.heading);
+    Serial.print(F(";    "));
+    Serial.print(F("Acceleration [m/s12]  X: ")); 
+    Serial.print(accel_event.acceleration.x); Serial.print(";  ");
+    Serial.print(F("Y: ")); Serial.print(accel_event.acceleration.y); Serial.print(";  ");
+    Serial.print(F("Z: ")); Serial.print(accel_event.acceleration.z); Serial.print(";     ");
+  }
+
+    // check response status
+    if (! ble.waitForOK() ) {
+      Serial.println(F("Failed to send?"));
+    }
+  
+  // Some data was found, its in the buffer
+  Serial.print(F("[Buffer ble] ")); 
+  Serial.println(ble.buffer);
+  ble.waitForOK();
 }
